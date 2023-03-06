407bbb906858f848dcd7d92a4ef8595da4faad56
onser23 <semed.semedzade@yahoo.com>
2023-03-06 16:30:10 +0400

    yy

--- App.js
+++ App.js
@@ -7,13 +7,13 @@ import React, {useEffect} from 'react';
 // import PushNotificationIOS from '@react-native-community/push-notification-ios';
 
 const App = () => {
-  useEffect(() => {
-    messaging().setBackgroundMessageHandler(async remoteMessage => {});
-    const unsubscribe = messaging().onMessage(async remoteMessage => {
-      console.log(remoteMessage);
-    });
-    return unsubscribe;
-  }, []);
+  // useEffect(() => {
+  //   messaging().setBackgroundMessageHandler(async remoteMessage => {});
+  //   const unsubscribe = messaging().onMessage(async remoteMessage => {
+  //     console.log(remoteMessage);
+  //   });
+  //   return unsubscribe;
+  // }, []);
   // const checkToken = async () => {
   //   const fcmToken = await messaging().getToken();
   //   if (fcmToken) {
@@ -54,79 +54,79 @@ const App = () => {
   // Must be outside of any component LifeCycle (such as `componentDidMount`).
 
   // Handle incoming notifications
-  PushNotification.createChannel(
-    {
-      channelId: 'fcm_fallback_notification_channel', // (required)
-      channelName: 'fcm_fallback_notification_channel', // (required)
-      channelDescription: 'A channel to categorise your notifications', // (optional) default: undefined.
-      playSound: true, // (optional) default: true
-      soundName: 'default', // (optional) See `soundName` parameter of `localNotification` function
-      importance: Importance.HIGH, // (optional) default: Importance.HIGH. Int value of the Android notification importance
-      vibrate: true, // (optional) default: true. Creates the default vibration pattern if true.
-    },
-    created => console.log(`createChannel returned '${created}'`), // (optional) callback returns whether the channel was created, false means it already existed.
-  );
+  // PushNotification.createChannel(
+  //   {
+  //     channelId: 'fcm_fallback_notification_channel', // (required)
+  //     channelName: 'fcm_fallback_notification_channel', // (required)
+  //     channelDescription: 'A channel to categorise your notifications', // (optional) default: undefined.
+  //     playSound: true, // (optional) default: true
+  //     soundName: 'default', // (optional) See `soundName` parameter of `localNotification` function
+  //     importance: Importance.HIGH, // (optional) default: Importance.HIGH. Int value of the Android notification importance
+  //     vibrate: true, // (optional) default: true. Creates the default vibration pattern if true.
+  //   },
+  //   created => console.log(`createChannel returned '${created}'`), // (optional) callback returns whether the channel was created, false means it already existed.
+  // );
 
-  PushNotification.configure({
-    onNotification: notification => {
-      console.log(notification);
-      // Display the notification
-      PushNotification.localNotification({
-        title: notification.title,
-        message: notification.message,
-        color: 'red',
-        ignoreInForeground: false,
-      });
-    },
+  // PushNotification.configure({
+  //   onNotification: notification => {
+  //     console.log(notification);
+  //     // Display the notification
+  //     PushNotification.localNotification({
+  //       title: notification.title,
+  //       message: notification.message,
+  //       color: 'red',
+  //       ignoreInForeground: false,
+  //     });
+  //   },
 
-    // (optional) Called when Token is generated (iOS and Android)
-    onRegister: function (token) {
-      console.log('TOKEN:', token);
-    },
+  //   // (optional) Called when Token is generated (iOS and Android)
+  //   onRegister: function (token) {
+  //     console.log('TOKEN:', token);
+  //   },
 
-    // (required) Called when a remote is received or opened, or local notification is opened
-    onNotification: function (notification) {
-      console.log('NOTIFICATION:', notification);
+  //   // (required) Called when a remote is received or opened, or local notification is opened
+  //   onNotification: function (notification) {
+  //     console.log('NOTIFICATION:', notification);
 
-      // process the notification
+  //     // process the notification
 
-      // (required) Called when a remote is received or opened, or local notification is opened
-      // notification.finish(PushNotificationIOS.FetchResult.NoData);
-    },
+  //     // (required) Called when a remote is received or opened, or local notification is opened
+  //     // notification.finish(PushNotificationIOS.FetchResult.NoData);
+  //   },
 
-    // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
-    onAction: function (notification) {
-      console.log('ACTION:', notification.action);
-      console.log('NOTIFICATION:', notification);
+  //   // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
+  //   onAction: function (notification) {
+  //     console.log('ACTION:', notification.action);
+  //     console.log('NOTIFICATION:', notification);
 
-      // process the action
-    },
+  //     // process the action
+  //   },
 
-    // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
-    onRegistrationError: function (err) {
-      console.error(err.message, err);
-    },
+  //   // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
+  //   onRegistrationError: function (err) {
+  //     console.error(err.message, err);
+  //   },
 
-    // IOS ONLY (optional): default: all - Permissions to register.
-    permissions: {
-      alert: true,
-      badge: true,
-      sound: true,
-    },
+  //   // IOS ONLY (optional): default: all - Permissions to register.
+  //   permissions: {
+  //     alert: true,
+  //     badge: true,
+  //     sound: true,
+  //   },
 
-    // Should the initial notification be popped automatically
-    // default: true
-    popInitialNotification: true,
+  //   // Should the initial notification be popped automatically
+  //   // default: true
+  //   popInitialNotification: true,
 
-    /**
-     * (optional) default: true
-     * - Specified if permissions (ios) and token (android and ios) will requested or not,
-     * - if not, you must call PushNotificationsHandler.requestPermissions() later
-     * - if you are not using remote notification or do not have Firebase installed, use this:
-     *     requestPermissions: Platform.OS === 'ios'
-     */
-    requestPermissions: true,
-  });
+  //   /**
+  //    * (optional) default: true
+  //    * - Specified if permissions (ios) and token (android and ios) will requested or not,
+  //    * - if not, you must call PushNotificationsHandler.requestPermissions() later
+  //    * - if you are not using remote notification or do not have Firebase installed, use this:
+  //    *     requestPermissions: Platform.OS === 'ios'
+  //    */
+  //   requestPermissions: true,
+  // });
 
   // PushNotification.getChannels(function (channel_ids) {
   //   console.log(channel_ids); // ['channel_id_1']
